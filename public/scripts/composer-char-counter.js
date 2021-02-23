2c97a129bef976c3b84fda33375706a96592f8eb
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-02-23 14:36:51 -0700

    Add dynamic behavior for the letter counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,14 @@
+let num = 100
 $(document).ready(() => {
-  $('.new-tweet textarea').on('keyup', () => {
-    console.log(this);
+  $('#tweet-text').on('keyup', (event) => {
+    let textLength = $(event.target).val().length;
+    let counterElem = $(event.target).next().find('output');
+    let counterValue = 140 - textLength;
+    counterElem.html(counterValue);
+    if (counterValue < 0) {
+      counterElem.addClass('toggleRed');
+    } else {
+      counterElem.removeClass('toggleRed');
+    }
   });
 });
