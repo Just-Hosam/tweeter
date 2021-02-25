7f250d7055ae0eff218234de934e1c1ca29af844
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-02-24 17:23:49 -0700

    add loadTweet function Send GET request to server and fetch data then render them

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,33 +1,3 @@
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "This is a test"
-    },
-    "created_at": 1614016490370
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1514102890370
-  }
-]
-
-// sorts tweet data based on created_at value
-const sortedData = tweetData.sort((a, b) => {
-  return b.created_at - a.created_at;
-});
-
 const renderTweets = function(tweets) {
   for (const tweet of tweets) {
     console.log('test');
@@ -52,28 +22,51 @@ const findDate = date => {
 
 const createTweetElement = function(tweet) {
   return `
-    <article class="tweet-container">
-      <header>
-        <div>
-          <img src=${tweet.user.avatars}>
-          <p class="user-name">${tweet.user.name}</p>
-        </div>
-        <p class="user-ID">${tweet.user.handle}</p>
-      </header>
-      <div class="tweet-content">
-        <p>${tweet.content.text}</p>
-      </div>
-      <footer>
-        <p>${dateDiff} ago</p>
-        <div class="icons">
-          <span>&#10084</span>
-          <span>&#8617</span>
-          <span>&#9873</span>
-        </div>
-      </footer>
-    </article>`;
+  <article class="tweet-container">
+  <header>
+  <div>
+  <img src=${tweet.user.avatars}>
+  <p class="user-name">${tweet.user.name}</p>
+  </div>
+  <p class="user-ID">${tweet.user.handle}</p>
+  </header>
+  <div class="tweet-content">
+  <p>${tweet.content.text}</p>
+  </div>
+  <footer>
+  <p>${findDate(tweet.created_at)} ago</p>
+  <div class="icons">
+  <span>&#10084</span>
+  <span>&#8617</span>
+  <span>&#9873</span>
+  </div>
+  </footer>
+  </article>`;
 }
 
 $(document).ready(function() {
-  renderTweets(sortedData);
+  const loadTweets = function() {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET'
+    }).then(data => {
+      const sortedData = data.sort((a, b) => {
+        return b.created_at - a.created_at;
+      });
+      renderTweets(sortedData);
+    })
+  };
+
+  $('#test').on('submit', function(event) {
+    event.preventDefault();
+    const str = $('#test').serialize();
+    $.ajax({
+      data: str,
+      url: '/tweets',
+      method: 'POST'
+    }).then(data => console.log('Good POST'));
+  });
+
+  loadTweets();
+  
 });
